9422e5b2bfe2851b56d6f9f5199bda4bc8a04a26
Kruceo <rafa2015krueger@gmail.com>
2022-11-30 17:02:46 -0300

    fix: default min style applied

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-let round = 10
+
 let width = "fit-content"
 let height = "fit-content"
 let lifeTime = 5000
-let padding = 10
+let padding = 10+'px'
 let notifications = []
-let stack = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+let stack = [0, 0, 0, 0, 0, 0]
 function spawn(title, message, options) {
     let obj = { el: null, stackPos: null, killed: false }
     let popupBody = document.createElement('div')
+
+    popupBody.setAttribute('class','notification '+(notifications.length+1))
+
     for (let i = 1; i <= stack.length; i++) {
         if (stack[i] == 0) {
             obj.stackPos = i
@@ -28,6 +31,7 @@ function spawn(title, message, options) {
     popupBody.appendChild(messageEl)
     popupBody.style.opacity = 0
     popupBody.style.transition = "all 250ms"
+    popupBody.style.padding = padding
     obj.el = popupBody
     notifications.push(obj)
     document.body.appendChild(popupBody)
@@ -39,18 +43,16 @@ function spawn(title, message, options) {
     popupBody.style.top = (window.visualViewport.height - popupBody.offsetHeight * obj.stackPos) + 'px'
     setTimeout(() => {
         popupBody.style.opacity = 0
-
         setTimeout(() => {
             popupBody.remove()
             stack[obj.stackPos] = 0
             obj.killed = true
         }, 250);
     }, options?.lifetime ?? lifeTime);
-
 }
 setInterval(() => {
     let freeCandidate = 999
-    
+
     notifications.sort((a, b) => a.stackPos - b.stackPos).forEach((each) => {
         if (!each || each.killed) {
             each = null;
@@ -62,7 +64,6 @@ setInterval(() => {
             if (stack[i] == 0) {
                 freeCandidate = i
                 if (each.stackPos > freeCandidate) {
-                    
                     stack[each.stackPos] = 0
                     each.stackPos = freeCandidate
                     stack[freeCandidate] = 1
@@ -71,14 +72,11 @@ setInterval(() => {
                 break
             }
         }
-        
-        
         return
-
     })
     console.log(notifications.sort((a, b) => a.stackPos - b.stackPos))
 
-}, 50);
+}, 150);
 
 for (let i = 0; i < 15; i++) {
     setTimeout(() => {
